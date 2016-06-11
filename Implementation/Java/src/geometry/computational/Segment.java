5683813363d72de0574ede31cce154ee3fd21d14
snate <sebastiano.valle@studenti.unipd.it>
2016-06-11 17:42:43 +0200

    Add Segment class

---
+++ Implementation/Java/src/geometry/computational/Segment.java
@@ -0,0 +1,22 @@
+package geometry.computational;
+
+/**
+ * Created by nate on 11/06/16.
+ */
+public class Segment {
+    private Point p1;
+    private Point p2;
+
+    public Segment(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public Point getP2() {
+        return p2;
+    }
+}
