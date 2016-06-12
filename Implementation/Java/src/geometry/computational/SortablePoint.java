e26858b768084cbe0cef7dd6899ee2c8be374c60
snate <sebastiano.valle@studenti.unipd.it>
2016-06-12 10:37:15 +0200

    Add Point's angular comparison

---
+++ Implementation/Java/src/geometry/computational/SortablePoint.java
@@ -0,0 +1,52 @@
+package geometry.computational;
+
+/**
+ * Point that provides O(1) angular comparison with other points.
+ */
+public class SortablePoint extends Point implements Comparable<Point> {
+
+    private Point origin;
+
+    private SortablePoint(int _x, int _y) {
+        super(_x, _y);
+    }
+
+    public SortablePoint withOrigin(Point o) {
+        origin = o;
+        return this;
+    }
+
+    @Override
+    public int compareTo(Point p) {
+        if(this.equals(p))
+            return 0;
+        if((getY() - origin.getY()) * (origin.getY() - p.getY()) > 0) {
+            // check who's above and who's not
+            if (getY() > p.getY())
+                return 1;
+            else
+                return -1;
+        }
+        // points are in the same halfplane
+        if(Library.angleLeft(origin, this, p) > 0)
+            return 1;
+        if(Library.angleLeft(origin, this, p) < 0)
+            return -1;
+        // points are collinear
+        if((getX() - origin.getX()) * (origin.getX() - p.getX()) > 0) {
+            // check who's above and who's not
+            if (getX() > p.getX())
+                return 1;
+            else
+                return -1;
+        }
+        // points are in the same halfline starting from origin
+        if(origin.sqrDistance(this) < origin.sqrDistance(p))
+            return 1;
+        return -1;
+    }
+
+    public static Point createSortablePoint(int x, int y, Point origin) {
+        return new SortablePoint(x,y).withOrigin(origin);
+    }
+}
