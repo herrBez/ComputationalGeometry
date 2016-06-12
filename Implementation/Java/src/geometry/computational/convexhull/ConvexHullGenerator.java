79fec4870e2c079fb6e98981aad4e13e444aba20
snate <sebastiano.valle@studenti.unipd.it>
2016-06-12 14:31:39 +0200

    Add ConvexHullGenerator interface

---
+++ Implementation/Java/src/geometry/computational/convexhull/ConvexHullGenerator.java
@@ -0,0 +1,9 @@
+package geometry.computational.convexhull;
+
+import geometry.computational.Polygon;
+
+/**
+ */
+public interface ConvexHullGenerator {
+    public Polygon generateConvexHull(Polygon polygon);
+}
